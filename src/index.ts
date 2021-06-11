e354b000231254131fdcfcea2b5952cf46d03fbf
the-1aw <bguilliod@gmail.com>
2021-06-11 15:34:19 +0200

    fix(index.js): wrong import for logger and config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from './app';
-import config from './config';
-import logger from './logger';
+import config from './utils/config';
+import logger from './utils/logger';
 
 app.listen(config.port, () => {
   logger.info(`Listening on port ${config.port}`);
