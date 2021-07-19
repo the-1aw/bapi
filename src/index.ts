3f206ba6b3851826eb0a811d63608d7a30e5cff9
the-1aw <bguilliod@gmail.com>
2021-07-19 19:31:30 +0200

    feat: add gracefull shutdown on sigterm

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,17 @@ import app from './app';
 import { appConfig } from './utils/config';
 import logger from './utils/logger';
 
-app.listen(appConfig.port, () => {
+const server = app.listen(appConfig.port, () => {
   logger.info(`Listening on port ${appConfig.port}`);
 });
+
+// Use this function to terminate everything that need to be close on shutdown
+// This is use to prevent server from dumping everything like it was trash
+const gracefullShutdown = () => {
+  logger.debug('SIGTERM signal received: closing HTTP server');
+  server.close(() => {
+    logger.debug('HTTP server closed');
+  });
+};
+
+process.on('SIGTERM', gracefullShutdown);
